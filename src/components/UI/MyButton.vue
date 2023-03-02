16133aff73334bc751f9a8dc62ead2bb8c70cf24
Anninka <annettech@yandex.ru>
2023-03-02 19:42:29 +1000

    Добавляет компоненты формы, поста, списка постов, компонент кнопки и инпута. Возможность добавлять комментарии

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <button class="btn" >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "my-button"
+}
+</script>
+
+<style scoped>
+.btn {
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: purple;
+  border: 1px solid purple;
+}
+
+.btn:hover {
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px purple;
+}  
+</style>
